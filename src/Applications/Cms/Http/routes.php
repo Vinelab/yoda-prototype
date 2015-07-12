03409dc32fe0259138efa76edade0ba3404ecfde
Abed Halawi <halawi.abed@gmail.com>
2015-07-12 12:43:20 +0300

    name the routes

--- src/Applications/Cms/Http/routes.php
+++ src/Applications/Cms/Http/routes.php
@@ -1,6 +1,13 @@
 <?php
 
 Route::group(['namespace' => 'Sample\Applications\Cms\Http\Controllers'], function() {
-    Route::get('articles/create', 'ArticleController@create');
-    Route::post('articles/store', 'ArticleController@store');
+    Route::get('articles/create', [
+        'as' => 'articles.create',
+        'uses' => 'ArticleController@create',
+    ]);
+
+    Route::post('articles/store', [
+        'as' => 'articles.store',
+        'uses' => 'ArticleController@store',
+    ]);
 });
